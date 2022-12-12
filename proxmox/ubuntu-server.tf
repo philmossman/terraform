cea33a343e5e41717c27614695053ba9a677ff07
Phil Mossman <mossmanpik@iname.com>
2022-12-12 12:41:54 +0000

    personalised the template

--- proxmox/ubuntu-server.tf
+++ proxmox/ubuntu-server.tf
@@ -1,36 +1,42 @@
 # Proxmox Full-Clone
 # ---
-# Create a new VM from a clone
+# Create a new Ubuntu Server from a packer clone
 
 resource "proxmox_vm_qemu" "your-vm" {
 
   # VM General Settings
-  target_node = "your-proxmox-node"
-  vmid        = "100"
-  name        = "vm-name"
-  desc        = "Description"
+  target_node = "pve"
+  vmid        = "200"
+  name        = "ubuntu-server"
+  desc        = "Ubuntu Server from packer generated clone"
 
   # VM Advanced General Settings
   onboot = true
 
   # VM OS Settings
-  clone = "your-clone"
+  clone = "ubuntu-server-jammy"
 
   # VM System Settings
   agent = 1
 
   # VM CPU Settings
-  cores   = 1
+  cores   = 2
   sockets = 1
   cpu     = "host"
 
   # VM Memory Settings
-  memory = 1024
+  memory = 4096
 
   # VM Network Settings
   network {
     bridge = "vmbr0"
     model  = "virtio"
+    tag = "23"
+  }
+
+  variable "ssh_pub_key" {
+  type = string
+  sensitive = true
   }
 
   # VM Cloud-Init Settings
@@ -40,10 +46,10 @@ resource "proxmox_vm_qemu" "your-vm" {
   # ipconfig0 = "ip=0.0.0.0/0,gw=0.0.0.0"
 
   # (Optional) Default User
-  # ciuser = "your-username"
+  ciuser = "phil"
 
   # (Optional) Add your SSH KEY
-  # sshkeys = <<EOF
-  # #YOUR-PUBLIC-SSH-KEY
-  # EOF
+  sshkeys = <<EOF
+  var.ssh_pub_key
+  EOF
 }
