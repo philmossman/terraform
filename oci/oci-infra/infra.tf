63a76683c49bf8d24c3cd5ea26e11a9354b4c87c
Phil Mossman <mossmanpik@iname.com>
2023-02-07 12:31:30 +0000

    reduce to two nodes

--- oci/oci-infra/infra.tf
+++ oci/oci-infra/infra.tf
@@ -203,7 +203,7 @@ resource "oci_containerengine_node_pool" "k8s_node_pool" {
       availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
       subnet_id           = oci_core_subnet.vcn_private_subnet.id
     }
-    size = 3
+    size = 2
 
 
   }
